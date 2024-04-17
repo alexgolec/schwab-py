fcaab875d2a362894006bc9f17c1d764bff3e072
Alex Golec <goleca@gmail.com>
2024-04-17 07:19:15 -0700

    Import orders at top level

--- schwab/__init__.py
+++ schwab/__init__.py
@@ -2,7 +2,7 @@ from . import auth
 from . import client
 #from . import contrib
 from . import debug
-#from . import orders
+from . import orders
 #from . import streaming
 
 from .version import version as __version__
