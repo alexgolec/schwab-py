cbd21ca467be5489d1a5a54a443a410ced39c79c
SkyHyperV <krypt0kurrenc@gmail.com>
2024-04-21 19:12:17 -0500

    - updated option parsing for Schwab style options symbols

--- schwab/orders/options.py
+++ schwab/orders/options.py
@@ -6,27 +6,25 @@ from schwab.orders.generic import OrderBuilder
 def _parse_expiration_date(expiration_date):
     date = None
     try:
-        date = datetime.datetime.strptime(expiration_date, '%m%d%y')
+        date = datetime.datetime.strptime(expiration_date, '%y%m%d')
         return datetime.date(year=date.year, month=date.month, day=date.day)
     except ValueError:
         pass
 
     raise ValueError(
         'expiration date must follow format ' +
-        '[Month with leading zero][Day with leading zero]' +
-        '[two digit year]')
+        '[YYmmdd] with leading zeroes for months or days < 10 i.e. 240420 for Apr 20, 2024 or 241001 for Oct 1, 2024')
 
 
 class OptionSymbol:
     '''
     Construct an option symbol from its constituent parts. Options symbols
-    have the following format: ``[Underlying]_[Two digit month][Two digit
-    day][Two digit year]['P' or 'C'][Strike price]``. Examples include:
+    have the following format: ``[Underlying][2 spaces][Two digit year][Two digit
+    month][Two digit day]['P' or 'C'][Strike price] -- Strike Price is multiple by 1000 and pre-pended with two zeroes if < 1000 strike or one zero if > 1000 ``. Examples include:
 
-     * ``GOOG_012122P620``: GOOG Jan 21 2022 620 Put
-     * ``TSLA_112020C1360``: TSLA Nov 20 2020 1360 Call
-     * ``SPY_121622C335``: SPY Dec 16 2022 335 Call
-
+     * ``QQQ  240420P00500000``: QQQ Apr 20, 2024 500 Call (note the two zeroes in front because strike is < 1000)
+     * ``SPXW  240420C05040000``: SPX Weekly Apr 20, 2024 5040 Call (note the one zero in front because strike is > 1000)
+     
     Note while each of the individual parts is validated by itself, the
     option symbol itself may not represent a traded option:
 
@@ -41,8 +39,8 @@ class OptionSymbol:
     :param underlying_symbol: Symbol of the underlying. Not validated.
     :param expiration_date: Expiration date. Accepts ``datetime.date``,
                             ``datetime.datetime``, or strings with the
-                            format ``[Two digit month][Two digit day][Two
-                            digit year]``.
+                            format ``[Two digit year][Two digit month][Two
+                            digit day]``.
     :param contract_type: ``P` or `PUT`` for put and ``C` or `CALL`` for call.
     :param strike_price_as_string: Strike price, represented by a string as
                                    you would see at the end of a real option
@@ -72,8 +70,8 @@ class OptionSymbol:
             self.expiration_date = expiration_date
         else:
             raise ValueError(
-                'expiration_date must be a string with format %m%d%y ' +
-                '(e.g. 01092020) or one of datetime.date or ' +
+                'expiration_date must be a string with format %y%m%d ' +
+                '(e.g. 240420 [Apr 20, 2024]) or one of datetime.date or ' +
                 'datetime.datetime')
 
         assert(isinstance(self.expiration_date, datetime.date))
@@ -89,32 +87,39 @@ class OptionSymbol:
                 'Strike price must be a string representing a positive ' +
                 'float')
 
-        # Remove extraneous zeroes at the end
-        strike_copy = strike_price_as_string
-        while strike_copy[-1] == '0':
-            strike_copy = strike_copy[:-1]
-        if strike_copy[-1] == '.':
-            strike_price_as_string = strike_copy[:-1]
+        # convert strike to Schwab's format
+        # Examples: 
+        # QQQ  240424P00500000
+        # SPXW  240422C05040000
+        if int(strike_price_as_string) < 1000:
+            self.strike = '00' + int(strike_price_as_string) * 1000
+        else:
+            self.strike = '0' + int(strike_price_as_string) * 1000
 
         self.strike_price = strike_price_as_string
 
     @classmethod
     def parse_symbol(cls, symbol):
         '''
-        Parse a string option symbol of the for ``[Underlying]_[Two digit month]
-        [Two digit day][Two digit year]['P' or 'C'][Strike price]``.
+        If Strike price < 1000:
+            Parse a string option symbol of the for ``[Underlying]  [Two digit year]
+            [Two digit month][Two digit day]['P' or 'C'][Strike price] * 1000 and prepend 2x (0s)``.
+        Else (Strike is >= 1000)
+            Parse a string option symbol of the for ``[Underlying]  [Two digit year]
+            [Two digit month][Two digit day]['P' or 'C'][Strike price] * 1000 and prepend 1x (0s)``.
+        
         '''
         format_error_str = (
             'option symbol must have format ' +
-            '[Underlying]_[Expiration][P/C][Strike]')
+            '[Underlying][2 spaces][Expiration in YYmmdd][P/C][Strike] with correct amount of pre and post-pended zeroes')
 
         # Underlying
         try:
-            underlying, rest = symbol.split('_')
+            underlying, rest = symbol.split('  ')
         except ValueError:
             underlying, rest = None, None
         if underlying is None:
-            raise ValueError('option symbol missing underscore \'_\', ' +
+            raise ValueError('option symbol missing underscore two spaces, ' +
                              format_error_str)
 
         # Expiration
@@ -134,15 +139,20 @@ class OptionSymbol:
 
         expiration_date = _parse_expiration_date(expiration_date)
 
+        # handle adjusting strike values based on Schwab's formatting, we need to take formats like:
+        # "QQQ  240424P00500000" results in strike 500
+        # "SPXW  240422C05040000" results in strike 5040
+        strike = int(strike / 1000)
+
         return OptionSymbol(underlying, expiration_date, contract_type, strike)
 
     def build(self):
         '''
         Returns the option symbol represented by this builder.
         '''
-        return '{}_{}{}{}'.format(
+        return '{}  {}{}{}'.format(
             self.underlying_symbol,
-            self.expiration_date.strftime('%m%d%y'),
+            self.expiration_date.strftime('%y%m%d'),
             self.contract_type,
             self.strike_price
         )
