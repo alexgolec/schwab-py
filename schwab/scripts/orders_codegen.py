dede66d3e722808b7bcb2723e10f090c0e9236f3
bosman81 <bahaaosman@gmail.com>
2024-05-06 21:27:18 -0700

    Necessary files and changes to enable order codegen feature

---
+++ schwab/scripts/orders_codegen.py
@@ -0,0 +1,44 @@
+import argparse
+import json
+
+from schwab.auth import client_from_token_file
+from schwab.contrib.orders import construct_repeat_order, code_for_builder
+
+
+def latest_order_main(sys_args):
+    parser = argparse.ArgumentParser(
+            description='Utilities for generating code from historical orders')
+
+    required = parser.add_argument_group('required arguments')
+    required.add_argument(
+            '--token_file', required=True, help='Path to token file')
+    required.add_argument('--api_key', required=True)
+    required.add_argument('--api_secret', required=True)
+
+    parser.add_argument('--account_hash', type=int,
+            help='Restrict lookups to a specific account ID')
+
+    args = parser.parse_args(args=sys_args)
+    client = client_from_token_file(args.token_file, args.api_key, args.api_secret)
+    hash = client.get_account_numbers().json()[0]['hashValue']
+
+    if args.account_hash:
+        orders = client.get_orders_for_account(args.account_hash).json()
+        if 'error' in orders:
+            print(('TDA returned error: "{}", This is most often caused by ' +
+                   'an invalid account ID').format(orders['error']))
+            return -1
+    else:
+        orders = client.get_orders_for_all_linked_accounts().json() 
+        if 'error' in orders:
+            print('TDA returned error: "{}"'.format(orders['error']))
+            return -1
+
+    if orders:
+        order = sorted(orders, key=lambda o: -o['orderId'])[0]
+        print('# Order ID', order['orderId'])
+        print(code_for_builder(construct_repeat_order(order)))
+    else:
+        print('No recent orders found')
+
+    return 0
