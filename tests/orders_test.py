df4bee19d70c82a6863c7cb371e9078c7c05625b
Alex Golec <goleca@gmail.com>
2024-05-10 09:00:24 -0400

    orders test

---
+++ tests/orders_test.py
@@ -0,0 +1,142 @@
+import unittest
+
+from schwab.orders.common import *
+from schwab.orders.equities import *
+from .utils import has_diff, no_duplicates
+
+from unittest.mock import patch
+
+
+class BuilderTemplates(unittest.TestCase):
+
+    def test_equity_buy_market(self):
+        self.assertFalse(has_diff({
+            'orderType': 'MARKET',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'BUY',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_buy_market('GOOG', 10).build()))
+
+    def test_equity_buy_limit(self):
+        self.assertFalse(has_diff({
+            'orderType': 'LIMIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'price': '199.99',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'BUY',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_buy_limit('GOOG', 10, 199.99).build()))
+
+    def test_equity_sell_market(self):
+        self.assertFalse(has_diff({
+            'orderType': 'MARKET',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'SELL',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_sell_market('GOOG', 10).build()))
+
+    def test_equity_sell_limit(self):
+        self.assertFalse(has_diff({
+            'orderType': 'LIMIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'price': '199.99',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'SELL',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_sell_limit('GOOG', 10, 199.99).build()))
+
+    def test_equity_sell_short_market(self):
+        self.assertFalse(has_diff({
+            'orderType': 'MARKET',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'SELL_SHORT',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_sell_short_market('GOOG', 10).build()))
+
+    def test_equity_sell_short_limit(self):
+        self.assertFalse(has_diff({
+            'orderType': 'LIMIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'price': '199.99',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'SELL_SHORT',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_sell_short_limit('GOOG', 10, 199.99).build()))
+
+    def test_equity_buy_to_cover_market(self):
+        self.assertFalse(has_diff({
+            'orderType': 'MARKET',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'BUY_TO_COVER',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_buy_to_cover_market('GOOG', 10).build()))
+
+    def test_equity_buy_to_cover_limit(self):
+        self.assertFalse(has_diff({
+            'orderType': 'LIMIT',
+            'session': 'NORMAL',
+            'duration': 'DAY',
+            'price': '199.99',
+            'orderStrategyType': 'SINGLE',
+            'orderLegCollection': [{
+                'instruction': 'BUY_TO_COVER',
+                'quantity': 10,
+                'instrument': {
+                    'symbol': 'GOOG',
+                    'assetType': 'EQUITY',
+                }
+            }]
+        }, equity_buy_to_cover_limit('GOOG', 10, 199.99).build()))
