c6056f65635bd29d43dacffce3153ff69721ca67
Alex Golec <goleca@gmail.com>
2024-05-14 13:07:16 -0400

    attempt to fix windows failures and add test for failure to get orders

--- tests/scripts/orders_codegen_test.py
+++ tests/scripts/orders_codegen_test.py
@@ -326,12 +326,42 @@ class LatestOrderTest(unittest.TestCase):
                 call('# Order ID', 401),
                 call(mock_code_for_builder.return_value)])
 
+
+    @no_duplicates
+    @patch('builtins.print')
+    @patch('schwab.scripts.orders_codegen.client_from_token_file')
+    @patch('schwab.scripts.orders_codegen.construct_repeat_order')
+    @patch('schwab.scripts.orders_codegen.code_for_builder')
+    def test_order_fetching_fails(
+            self,
+            mock_code_for_builder,
+            mock_construct_repeat_order,
+            mock_client_from_token_file,
+            mock_print):
+        self.add_arg('--token_file')
+        self.add_arg('filename.json')
+        self.add_arg('--api_key')
+        self.add_arg('api-key')
+        self.add_arg('--app_secret')
+        self.add_arg('app-secret')
+
+        mock_client = MagicMock()
+        mock_client_from_token_file.return_value = mock_client
+        mock_client.get_orders_for_all_linked_accounts.return_value \
+                = httpx.Response(400)
+
+        self.assertEqual(self.main(), -1)
+
+        mock_print.assert_called_once_with(
+                AnyStringWith('Returned HTTP status code 400'))
+
+
+
 class ScriptInvocationTest(unittest.TestCase):
 
     def test_get_help(self):
-        res = subprocess.run(
+        output = subprocess.check_output(
                 'schwab-order-codegen.py --help',
-                shell=True, check=True, text=True, capture_output=True)
-        print(res.stdout)
+                shell=True, text=True)
         self.assertIn('Utilities for generating code from historical orders',
-                      res.stdout)
+                      output)
